a09ea99235a51b3d94b06cc3a0dc31d21a1978f2
AliGadoo <aligado232@gmail.com>
2025-10-31 16:42:48 +0200

    implements dropdown

--- Frontend/src/Components/SearchBar/SearchBar.jsx
+++ Frontend/src/Components/SearchBar/SearchBar.jsx
@@ -1,9 +1,74 @@
+import { useState } from "react";
 import Arrow_Down from "../../assets/icons/Arrow_Down.png";
 import Search_icon from "../../assets/icons/Search_icon.png";
 
 const SearchBar = () => {
+  const cityOptions = [
+    "Cairo",
+    "Alexandria",
+    "Giza",
+    "Sharm El-Sheikh",
+    "Luxor",
+  ];
+
+  const locationOptions = {
+    Cairo: [
+      "Nasr City",
+      "Heliopolis",
+      "Maadi",
+      "Zamalek",
+      "New Cairo",
+      "Downtown",
+    ],
+    Alexandria: ["Stanley", "Smouha", "Gleem", "Miami", "Sidi Gaber"],
+    Giza: [
+      "Dokki",
+      "Mohandessin",
+      "6th of October City",
+      "Haram",
+      "Sheikh Zayed",
+    ],
+    "Sharm El-Sheikh": ["Naama Bay", "Hadaba", "Sharks Bay", "Ras Um Sid"],
+    Luxor: ["East Bank", "West Bank", "Karnak", "El Tod"],
+  };
+
+  const [showCities, setShowCities] = useState(false);
+  const [selectedCity, setSelectedCity] = useState("City");
+
+  const [showLocations, setShowLocations] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState("Location");
+
+  const [showError, setShowError] = useState(false);
+
+  const handleCountryDropdown = () => {
+    setShowCities(!showCities);
+    setShowLocations(false);
+  };
+
+  const handleLocationDropdown = () => {
+    if (selectedCity !== "City") {
+      setShowLocations(!showLocations);
+      setShowCities(false);
+      setShowError(false);
+    } else {
+      setShowError(true);
+    }
+  };
+
+  const handleSelectCity = (city) => {
+    setSelectedCity(city);
+    setSelectedLocation("Location");
+    setShowCities(false);
+    setShowError(false);
+  };
+
+  const handleSelectLocation = (location) => {
+    setSelectedLocation(location);
+    setShowCities(false);
+  };
+
   return (
-    <div className="mx-4 sm:mx-8 px-6 sm:px-8 py-8 bg-light-neutral rounded-xl mt-10 sm:mt-20 inner-shadow flex flex-col lg:flex-row lg:items-center flex-wrap items-stretch gap-4 sm:gap-6 max-w-6xl w-full">
+    <div className="mx-4 sm:mx-8 px-6 sm:px-8 py-8 bg-light-neutral rounded-xl mt-5 sm:mt-10 inner-shadow flex flex-col-reverse lg:flex-row lg:items-center flex-wrap items-stretch gap-4 sm:gap-6 max-w-6xl w-full">
       <div className="flex gap-6 sm:flex-row flex-col ">
         <div className="bg-state-blue text-light-neutral px-1 py-1 rounded-xl flex-1 xl:max-w-[180px] flex items-center justify-center">
           <input
@@ -12,13 +77,58 @@ const SearchBar = () => {
           />
         </div>
 
-        <div className="bg-state-blue text-light-neutral flex items-center justify-between px-4  py-2 rounded-xl gap-2 xl:flex-none flex-1  min-w-[140px] cursor-pointer">
-          <span className="text-sm sm:text-base">Country</span>
+        <div
+          onClick={handleCountryDropdown}
+          className="bg-state-blue text-light-neutral flex items-center justify-between px-4  py-2 rounded-xl gap-2 xl:flex-none flex-1  min-w-[180px] cursor-pointer relative"
+        >
+          {showCities && (
+            <div className="absolute bg-white w-full left-0 top-full text-state-blue z-50 rounded-xl overflow-hidden">
+              <ul className="flex flex-col gap-2 py-2">
+                {cityOptions.map((city) => (
+                  <li
+                    onClick={() => {
+                      handleSelectCity(city);
+                    }}
+                    key={city}
+                    className="px-4 py-2"
+                  >
+                    {city}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          <span className="text-sm sm:text-base">{selectedCity}</span>
           <img src={Arrow_Down} alt="arrow_Down icon" className="w-4 sm:w-5" />
         </div>
 
-        <div className="bg-state-blue text-light-neutral flex items-center justify-between px-4  py-2 rounded-xl gap-2 xl:flex-none flex-1  min-w-[140px] cursor-pointer">
-          <span className="text-sm sm:text-base">Location</span>
+        <div
+          onClick={handleLocationDropdown}
+          className="bg-state-blue text-light-neutral flex items-center justify-between px-4  py-2 rounded-xl gap-2 xl:flex-none flex-1  min-w-[180px] cursor-pointer relative"
+        >
+          {showError && (
+            <p className="text-red-600 text-sm absolute top-[-20px] left-2">
+              Select city first
+            </p>
+          )}
+          {showLocations && locationOptions[selectedCity] && (
+            <div className="absolute bg-white w-full left-0 top-full text-state-blue z-50 rounded-xl overflow-hidden">
+              <ul className="flex flex-col gap-2 py-2">
+                {locationOptions[selectedCity].map((location) => (
+                  <li
+                    onClick={() => {
+                      handleSelectLocation(location);
+                    }}
+                    key={location}
+                    className="px-4 py-2"
+                  >
+                    {location}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          <span className="text-sm sm:text-base">{selectedLocation}</span>
           <img src={Arrow_Down} alt="arrow_Down icon" className="w-4 sm:w-5" />
         </div>
       </div>
